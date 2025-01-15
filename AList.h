b7cc82a1650a3b3f695612f404880203760a8cfa
MoskowitzRocket <kalebug@gmail.com>
2025-01-14 16:08:46 -0800

    Working on functions, shit breaks when adding stuff beyond theCapacity

--- AList.h
+++ AList.h
@@ -33,35 +33,79 @@ class AList
     void addLeft( const Object x )// Insert a new object at the left end 
     {
 	// Implement this....
+        theSize ++;
+        objects[left] = x;
+        left = (left - 1 + theCapacity) % theCapacity;
+
     }
 
     void addRight( const Object x )// Insert a new object at the right end
     {
         theSize ++ ;
-	objects[right] = x ;
+	      objects[right] = x ;
         right = (right+1) % theCapacity ;
     }
 
     Object removeRight( )// Remove and return the object at the right end
     {
         // Implement this 
+        theSize --;
+        Object t_Val = objects[right];
+        right = (right - 1 + theCapacity) % theCapacity;
+
+        return t_Val;
+
+        
     }
 
     Object removeLeft( )// Remove and return the object at the left end
     {
         // Implement this 
+        theSize--;
+        Object t_Val = objects[left];
+        left = (left + 1) % theCapacity;
+        return t_Val;
     }
 
     void display() const // print out the contents of the deque
     {
-       // Implement this.
+       if (empty()) {
+        std::cout << "AList is empty" << std::endl;
+      } 
+      // int index = (left + 1) % theCapacity; // Start from the logical first element
+      // for (int i = 0; i < theSize; ++i) {
+      //   std::cout << objects[index] << (i < theSize - 1 ? ", " : "");
+      //   index = (index + 1) % theCapacity; // Move to the next logical element
+      // }
+      // std::cout << std::endl;
+      std::cout << "Raw objects array: ";
+      for (int i = 0; i < theCapacity; ++i) {
+        std::cout << objects[i] << (i < theCapacity - 1 ? ", " : "x");
+      }
+      std::cout << std::endl;
+
+
     }
 
     void ddisplay() const // print out the contents of the objects 
     // array, and relevant variables, for debugging or verifying 
     // correctness. 
     {
-        // Implement this. 
+      // Implement this. 
+      std::cout << "theSize " << theSize << std::endl;
+      std::cout << "Left " << left << std::endl;
+      std::cout << "Right " << right << std::endl;
+      std::cout << "theCapacity " << theCapacity << std::endl;
+
+      if (empty()) {
+      std::cout << "AList is empty" << std::endl;
+      } 
+      int index = (left + 1) % theCapacity; // Start from the logical first element
+      for (int i = 0; i < theSize; ++i) {
+        std::cout << objects[index] << (i < theSize - 1 ? ", " : "");
+        index = (index + 1) % theCapacity; // Move to the next logical element
+      }
+      std::cout << std::endl;
     }
 
 
--- AListTest.cpp
+++ AListTest.cpp
@@ -5,5 +5,12 @@ int main(){
 
     std::cout << "Hello World" << std::endl;
 
+    AList<int> alist;
+
+    for (int i{1}; i <=14; i++){
+        alist.addLeft(i);
+        alist.ddisplay();
+    }   
+
     return 0;
 }
